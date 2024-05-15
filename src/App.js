2f38dc709fbe148f44ea8a93000fe02e1dd924a6
Harutyun <91012818+HarutyunTovmasyan19994@users.noreply.github.com>
2024-05-15 22:21:21 +0400

    change counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React,{useState} from "react";
+import {useState} from "react";
 
 function App() {
   const [state,setState] = useState(0)
 
-  const increment = setState(prev => prev + 1)
+  const increment =()=> {
+    setState(prev => prev + 1)
+  }
 
   return (
     <div>
-        <h1> value = {state}</h1>
-      <button onClick={increment}>
+        <h1 style={{fontSize:70,color:"red" }}> value = {state}</h1>
+      <button style={{background:"black", color:"white"}} onClick={increment}>
         increment
       </button>
     </div>
